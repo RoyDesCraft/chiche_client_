88958e2706bb37d54b9e7308ec8e98df7e97343a
Esu-xyz <eoldraw@gmail.com>
2025-11-01 22:56:20 +0100

    Log in / Sign up pop up not finished

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+// DOM Elements
 const sidebar = document.getElementById("sidebar");
 const sidebarContent = document.querySelector('.sidebar-content');
 const postButton = document.getElementById('post-button');
 const tabContent = document.getElementById('tab-content');
 const tabTextElements = document.querySelectorAll('.tab-text');
+const closeSidebarButton = document.querySelector(".close-sidebar");
 
 // INITIAL STATES
-
-sidebar.classList.add("sidebar-open"); // FIRST STATE
+sidebar.classList.add("sidebar-open");
 tabTextElements.forEach((element) => {
     element.classList.add("tab-text-sidebar-opened");
 });
 
-document.querySelector(".close-sidebar").addEventListener("click", () => {
+// Toggle Sidebar
+closeSidebarButton.addEventListener("click", () => {
     if (sidebar.classList.contains("sidebar-open")) {
         // CLOSING
         sidebar.classList.remove("sidebar-open");
@@ -25,7 +27,7 @@ document.querySelector(".close-sidebar").addEventListener("click", () => {
         tabTextElements.forEach((element) => {
             element.classList.remove("tab-text-sidebar-opened");
             element.classList.add("tab-text-sidebar-closed");
-        })
+        });
     } else {
         // OPENING
         sidebar.classList.remove("sidebar-closed");
@@ -39,31 +41,43 @@ document.querySelector(".close-sidebar").addEventListener("click", () => {
         tabTextElements.forEach((element) => {
             element.classList.remove("tab-text-sidebar-closed");
             element.classList.add("tab-text-sidebar-opened");
-        })
+        });
     }
 });
 
-/* LOGIN OVERLAY */
+// Login Overlay
 const loginOverlay = document.getElementById('login-overlay');
-loginOverlay.style.display = 'flex';
-history.pushState(null, '', '/login');
+const openLoginOverlayButton = document.getElementById('open-login-overlay-button');
+const closeLoginOverlayButton = document.getElementById('close-login-overlay-button');
 
-/* GOOGLE SIGN-IN */
-function handleCredentialResponse(response) {
-    console.log("ID Token: " + response.credential);
-    // Envoie le token à ton backend ici
-    // fetch('/auth/google', { method: 'POST', body: JSON.stringify({ token: response.credential }) });
-}
+openLoginOverlayButton.addEventListener('click', () => {
+    if (loginOverlay.classList.contains('login-overlay-open')) {
 
-window.onload = function () {
-    google.accounts.id.initialize({
-        client_id: "375435848419-ud1ao7ot13k81uh48gfvk3rbjo47hdce.apps.googleusercontent.com",
-        login_uri: "https://chiche-server.onrender.com/auth/google",
-        callback: handleCredentialResponse,
-        ux_mode: 'popup',
-    });
-    // Associe le bouton Google personnalisé
-    googleLoginButton.onclick = () => {
-        google.accounts.id.prompt();
-    };
-};
+        loginOverlay.classList.remove('login-overlay-open');
+        loginOverlay.classList.add('login-overlay-closed');
+        history.pushState(null, '', '/');
+    } else {
+
+    loginOverlay.style.display = 'flex';
+    history.pushState(null, '', '/login');
+
+    loginOverlay.classList.remove('login-overlay-closed');
+    loginOverlay.classList.add('login-overlay-open');
+    }
+});
+
+closeLoginOverlayButton.addEventListener('click', () => {
+    if (loginOverlay.classList.contains('login-overlay-open')) {
+
+        loginOverlay.classList.remove('login-overlay-open');
+        loginOverlay.classList.add('login-overlay-closed');
+        history.pushState(null, '', '/');
+    } else {
+
+    loginOverlay.style.display = 'flex';
+    history.pushState(null, '', '/login');
+
+    loginOverlay.classList.remove('login-overlay-closed');
+    loginOverlay.classList.add('login-overlay-open');
+    }
+});
