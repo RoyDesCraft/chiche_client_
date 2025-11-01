b0cb7c386b7fdbd95c32277db646062c22061bd7
Esu-xyz <eoldraw@gmail.com>
2025-11-01 02:35:21 +0100

    Responsive sidebar, tabs, post button, closing sidebar, scrollbar fixed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 document.querySelector('.close-sidebar').addEventListener('click', function() {
     document.getElementById('sidebar').classList.toggle('sidebar-closed');
+    document.getElementById('post-button').classList.toggle('post-button-sidebar-closed');
 });
