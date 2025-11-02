b5cbbd68b53c5a783a7bb23b8641268dfe946612
Roy ‎ <le.roy@gmail.com>
2025-11-02 15:53:30 +0100

    Added better login logic

--- main.js
+++ main.js
@@ -586,9 +586,10 @@ async function handleEmailLogin() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                username: username,
+                email: email,
                 password: password
             })
+
         });
 
         const data = await response.json();
